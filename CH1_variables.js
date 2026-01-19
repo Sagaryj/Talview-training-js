f1b455a33228f2b148c67248b85073db4c8bb1db
Sagar YJ <yjsagar@gmail.com>
2026-01-19 11:02:35 +0530

    Chapter1 variables

---
+++ CH1_variables.js
@@ -0,0 +1,71 @@
+let age = 25;
+console.log("age:", age);
+var name = "Sagar";
+console.log("name:", name);
+const isStudent = false;
+console.log("isStudent:", isStudent);
+console.log("age type:", typeof age);
+console.log("name type:", typeof name);
+console.log("isStudent type:", typeof isStudent);
+let score = 90;
+score = 95; // allowed
+console.log("Updated score:", score);
+const PI = 3.14159;
+console.log("PI:", PI);
+const user = { name: "Sagar" };
+user.name = "YJ"; // allowed
+console.log("Updated user name:", user.name);
+console.log("user name :", user);
+let value = 10;      // number
+value = "ten";      // string
+console.log("type of value:", typeof value);
+/* Primitive Data Types (let , var , const )
+
+Primitive types are:
+
+Immutable
+
+Stored directly in memory
+
+Compared by value*/
+/* primitive alli 4 types idhe
+
+1. number
+
+2. string
+
+3. boolean
+
+4. undefined
+5 null
+6 big int
+7 symbol    */  
+let string ="Hello, World!";
+console.log("string:", string[0]);
+let x;
+console.log(x); // undefined
+let z=null;
+console.log(z); 
+let big = 12345678901234567890n;
+console.log("big int:", typeof big);
+const id = Symbol("id");
+const id2 = Symbol("id");
+
+id === id2; // falses
+console.log("symbol:", id === id2);
+const person = {
+  names: "Sagara YJ",
+  age: 30,
+  names1: "Sagar",
+  age1: 25,
+};
+console.log("person:", person);
+
+console.log("person age:",person.age);
+typeof 10;           // "number"
+typeof "hello";      // "string"
+typeof true;         // "boolean"
+typeof undefined;    // "undefined"
+typeof null;         // "object" 
+typeof {};           // "object"
+typeof function(){}; // "function"
