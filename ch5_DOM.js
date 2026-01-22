a1e44d28010fbac4cd141d5bf891b419acd0b385
Sagar YJ <yjsagar@gmail.com>
2026-01-22 14:44:29 +0530

    Daily updation 22th jan

---
+++ ch5_DOM.js
@@ -0,0 +1,8 @@
+console.log(window);
+console.log(document);
+console.log(window.document);
+console.dir(document.body);
+console.dir(document);
+let heading = document.getElementById("heading");
+console.log(heading);
+
